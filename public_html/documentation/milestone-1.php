3ce616607005f2d45c57eb8686917a36f79641df
Marlan Ball <wyndows2k@earthlink.net>
2016-04-30 14:57:44 -0600

    added milestone-2 and images

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,23 +5,23 @@
 		<title>Wyn-Tech.com</title>
 	</head>
 	<body>
-			<h3>Wyn-Tech.com Milestone 1</h3>
-			<h4>Purpose, Audience and Goal</h4>
-			<p><strong>Purpose</strong>: This will be a website used to showcase my web development skills as well as my technical background.</p>
-			<p><strong>Audience</strong>: This site is targeted to IT and technical professionals, IT and technical hiring managers as well as peers.</p>
-			<p><strong>Goal</strong>: The primary goal of this site is to assist in getting a full time job. The secondary goal of this site is to showcase my work, and provide a
+			<h2>Wyn-Tech.com Milestone 1</h2>
+			<h3>Purpose, Audience and Goal</h3>
+			<p><strong>Purpose: </strong>This will be a website used to showcase my web development skills as well as my technical background.</p>
+			<p><strong>Audience: </strong>This site is targeted to IT and technical professionals, IT and technical hiring managers as well as peers.</p>
+			<p><strong>Goal: </strong>The primary goal of this site is to assist in getting a full time job. The secondary goal of this site is to showcase my work, and provide a
 				professional online presence.</p>
-			<h4>Persona</h4>
-			<p><strong>Name: </strong>Christa Lee<br><strong>Age: </strong>36</p>
+			<h3>Persona</h3>
+			<p><strong>Name: </strong>Christa Lee &nbsp &nbsp &nbsp &nbsp<strong>Age: </strong>36</p>
 			<p><strong>Profession: </strong>Office Manager of Decor8-on-a-dime, an established company that has been local in it's
 				business scope and desires to expand nationally using an online presence.  The are looking to hire a contract
 				developer to create their website Decor8-on-a-dime.com.</p>
 			<p><strong>Technology: </strong>Uses Windows and Microsoft Office at work and an Android phone for work/personal
 				use.</p>
-			<p><strong>Attitudes and Behaviors: </strong>Personable and likes to meet with people one-on-one.</p>
+			<p><strong>Attitudes and Behaviors: </strong>Personable and likes to meet with people one-on-one to discuss business.</p>
 			<p><strong>Frustrations and Needs: </strong>Decor8-on-a-dime needs to get the website up and running, but the timeline is open.  They are looking for a good fit for their needs and don't want to invest in the website unless they believe it would be a good use of their funds.</p>
 			<p><strong>Goals: </strong>Christa wants to see from the site if I will be an initial good fit for discussing their Decor8.com website with.  They are not interested in working with a do-it-yourself site builder.</p>
 			<h4>Use Case</h4>
-			<p>Heard from a friend about Wyn-Tech.com and checked the site and like the appearance and wanted to talk about their desires for Decor8-on-a-dime.com</p>
+			<p>Heard from a friend about Wyn-Tech.com and checked the site and liked the appearance and wanted to talk about their desires for Decor8-on-a-dime.com</p>
 	</body>
 </html>
