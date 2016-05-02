684d36c98f968196aab25182c27dcb789a64989e
Marlan Ball <wyndows2k@earthlink.net>
2016-05-02 06:45:13 -0600

    continued work with images and styles

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,7 +6,7 @@
 		<title>Wyn-Tech.com</title>
 	</head>
 	<body>
-		<h2>Wyn-Tech.com Milestone 2&alpha</h2>
+		<h2>Wyn-Tech.com Milestone 2&alpha;</h2>
 		<h3>Proposed Site Map</h3>
 		<p>Site will consist of 3 top-level pages total. No sub-pages or sub-navigation. This will be the Minimum Viable Product.</p>
 		<ul>
@@ -15,15 +15,15 @@
 			<li>Portfolio</li>
 		</ul>
 		<h3>Content Strategy</h3>
-		<p><strong>Home Page: </strong>A brief introduction that clearly states my name and role as a Web Developer, and will feature 2 calls to action that will direct the user to 		the About Me or the Portfolio page.</p>
-		<p><strong>About Me: </strong>This page will detail my professional history, core skill set, and professional highlights. This page will showcase my strengths and personality. 		I will feature a contrasting sidebar that lists my core skills which will be easily 		readable at a glance. The tone of voice will be personable and casual, as I am only 		interested in opportunities with individuals and organizations that I am a good 		personality fit for. This will be a moving text area as my contact info will be locked 		at	the bottom of the page</p>
+		<p><strong>Home Page: </strong>A brief introduction that introduces me and my role as a Web Developer, and will feature 2 calls to action that will direct the user to the About Me or the Portfolio page.</p>
+		<p><strong>About Me: </strong>This page will detail my professional history, core skill set, and professional highlights. This page will showcase my strengths and personality. The tone of voice will be personable and casual, as I am only interested in opportunities with individuals and organizations that I am a good personality fit for. This will be a moving text area as my contact info will be locked at the bottom of the page</p>
 		<p><strong>Portfolio: </strong>1 -2 examples of my past work, including my roles and contributions to each project. I want this to be an animated, responsive image gallery.</p>
 		<h3>Interaction Flow</h3>
 		<p>Christa is viewing the site on her phone. She goes to wyn-tech.com and is shown the Home Page. In the next 3 minutes, here is an outline of Christa’s interactions with the 		site:</p>
 		<ol>
 			<li>Lands on Home page. Touches menu and a dropdown shows with Home, About and Portfolio.</li>
 			<li>Views the two projects in Portfolio gallery. Finds work satisfactory. Navigates to the "About Me" page via the main navigation in the header.</li>
-			<li>1 minute look at the About Me page. Sees core skills featured in a contrasting side panel. The interaction stops here. Christa has decided, based on the information 			provided, that she will be contacting me regarding the website for Decor8-on-a-dime</li>
+			<li>1 minute look at the About Me page. Sees core skills featured in a contrasting side panel. The interaction stops here. Christa has decided, based on the information provided, that she will be contacting me regarding the website for Decor8-on-a-dime</li>
 		</ol>
 		<h3>Wireframes</h3>
 		<p>This site will feature 2 layouts:</p>
@@ -32,14 +32,21 @@
 			<li>Content Page Layout</li>
 		</ol>
 		<br>
-		<div
-		<h4>Home Page Layout: Mobile</h4>
-		<img class="img1" src="../media/mobile_home.svg" alt="home page mobile wireframe">
-		<h4>Content Page Layout: Mobile</h4>
-		<img class="img2" src="../media/mobile_content.svg" alt="content page mobile wireframe">
-		<h4>Home Page Layout: Desktop</h4>
-		<img class="img3" src="../media/desktop_home.svg" alt="home page desktop wireframe">
-		<h4>Content Page Layout: Desktop</h4>
-		<img class="img4" src="../media/desktop_content.svg" alt="content page desktop wireframe">
+		<div id = "homemobile">
+			<h4>Home Page Layout: Mobile</h4>
+			<img class="img1" src="../media/mobile-home.svg" alt="home page mobile wireframe">
+		</div>
+		<div id = "contentmobile">
+			<h4>Content Page Layout: Mobile</h4>
+			<img class="img2" src="../media/mobile-content.svg" alt="content page mobile 			wireframe">
+		</div>
+		<div id = "homedesktop">
+			<h4>Home Page Layout: Desktop</h4>
+			<img class="img3" src="../media/desktop-home.svg" alt="home page desktop wireframe">
+		</div>
+		<div id = "contentdesktop">
+			<h4>Content Page Layout: Desktop</h4>
+			<img class="img4" src="../media/desktop-content.svg" alt="content page desktop 			wireframe">
+		</div>
 	</body>
 </html>
